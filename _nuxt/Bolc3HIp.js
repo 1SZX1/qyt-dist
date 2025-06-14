8cb6200de4ecd24964c9751adef9bec7dd4ee8bb
啊拉拉 <2414126198@qq.com>
2025-06-15 00:19:11 +0800

    first commit

---
+++ _nuxt/Bolc3HIp.js
@@ -0,0 +1,8 @@
+import{u as l}from"./BZkE4UoL.js";import{e as i,c as a,a as t,F as m,r as p,o as n,t as o}from"./DFEYIJ3P.js";const c={class:"container pt40 pb20"},d={class:"flex flex-col items-center pxlg",md:"flex-row items-start"},g={md:"mlauto"},u={class:"text-lg fw-bold"},x={class:"c-primary mb1"},v=i({__name:"careers",setup(f){return l({title:"招贤纳士"}),(b,e)=>(n(),a("div",c,[e[2]||(e[2]=t("div",{class:"text-9 fw-bold text-center py54px c-primary",bg:"no-repeat center cover",style:{"background-image":"url(/imgs/title-bg.jpeg)"}}," 招贤纳士 ",-1)),e[3]||(e[3]=t("div",{class:"my50px h.5 bg-primary"},null,-1)),t("div",d,[e[1]||(e[1]=t("div",{class:"text-7 c-primary mb5 font-[AlimamaDongFangDaKai]"},"期待您的加入。",-1)),t("div",g,[e[0]||(e[0]=t("p",{class:"mb4 font-[AlimamaDongFangDaKai] text-7"},"小红书运营",-1)),t("ul",u,[(n(),a(m,null,p([{title:"职责内容：",content:`1.负责小红书平台的账号搭建和内容输出;
+2.有饮品类或酒品类运营经验者优先，能切中用户需求及喜好，增加咨询量;
+3.熟知小红书平台常见的拉新、裂变等活动玩法，搭建活动链路，并有复盘分析的能力；
+4.与公司其他部门及团队合作，顺利推进营销活动整体项目及完成上级安排的其他工作。`},{title:"职责内容：",content:`1.独立完成内容运营所需的选题策划，素材编辑、文案撰写等，对内容效果负责;
+2.对小红书内容营销有一定的研究，敏锐的洞察力
+3.语言表达及沟通能力强，能妥善维护与新媒体平台粉丝之间的关系
+4.有PS图形软件基础，熟练应用视频剪辑软件等优先
+5.全日制本科毕业，2年小红书平台运营经验`},{title:"联系电话：",content:"028-62070633"}],({title:s,content:r})=>t("li",{key:s,class:"not-last:mb8 ws-pre-wrap"},[t("p",x,o(s),1),t("p",null,o(r),1)])),64))])])])]))}});export{v as default};
